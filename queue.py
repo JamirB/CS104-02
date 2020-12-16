2e490e85ddb8a261a4cd5d9b8edc2a91409e5245
JamirB <72527334+JamirB@users.noreply.github.com>
2020-12-16 18:23:44 -0500

    Add files via upload
    queue

---
+++ queue.py
@@ -0,0 +1,9 @@
+
+ Names=["Jamir","Bill","Nas","Jack","Marc","Mike","Sam","Ray","Rylo","Don"]
+
+ for j in range(0, 10):
+     acceptedName = str(input("Please enter a name: "))
+     name.append(Names)
+
+ for j in range(0, len(names)):
+     print(names.pop(0))
